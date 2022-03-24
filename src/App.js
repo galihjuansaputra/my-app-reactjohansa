f5f9adcaaac86f71e6c1a552d23330dfbe7500ca
Galih Juansaputra <galih.juansaputra@gmail.com>
2022-03-24 23:25:12 +0800

    Menambahkan Produk

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="Parentbox">
-      <div className="Foto">
-        <img src="sneaker.jpg"/>
-      </div>
-
-      <div className="Deskripsi">
-        <h1 className="Judul">Dior Vibe</h1>
-        <p className="Desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil quam porro animi at aperiam qui fugit eligendi sint nostrum veniam eos vero ipsa, dolor sit laudantium quo reprehenderit culpa eaque?
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque repellat magnam et maiores itaque beatae adipisci animi eum eius ratione laudantium accusamus, tempore nisi assumenda fugiat officia aut labore deserunt.</p>
-      </div>
-
+      <FotoProduk />
+      <ProdukInfo isDiscount="coming" category="SNEAKER" name="Nike Air Zoom Pegasus" />
     </div>
   );
+}
+
+function FotoProduk() {
+  return (
+    <div className="Foto">
+      <img src="sneaker.jpg" />
+    </div>
+  );
+}
+
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+
+  if (isDiscount == "no") {
+    return <p>Discount 50% Off</p>;
+  } else if(isDiscount == "coming") {
+    return <p>Akan Ada Discount</p>;
+  } else {
+    return <p>Belum Ada Discount</p>;
+  }
+}
+
+function ProdukInfo(props) {
+  const { category, name, isDiscount } = props;
+
+  return (
+    <div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">{name}</h1>
+        <p className="Price">IDR 73.399.399</p>
+        <CheckDiscount isDiscount={isDiscount} />
+        <p className="Info">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
+          aliquam quam exercitationem blanditiis tenetur nihil molestias nam
+          odit laboriosam commodi ad illum libero repudiandae id voluptatum
+          dolore voluptate, officiis impedit.
+        </p>
+        <a onClick={(e) => TambahCart(name, e)} href="#">
+          Add to Cart
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function TambahCart(e) {
+  console.log("Membeli " + e);
 }
 
 export default App;
